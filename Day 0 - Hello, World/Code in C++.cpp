68cd61c77c5538ffa3ae41617d35e99c6229a1e9
Aseem Jain <jainaseem2004@gmail.com>
2023-10-18 18:19:04 +0530

    Day 0 - In C++

---
+++ Day 0 - Hello, World/Code in C++.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    // Declare a variable named 'input_string' to hold our input.
+    string input_string; 
+    
+    // Read a full line of input from stdin (cin) and save it to our variable, input_string.
+    getline(cin, input_string); 
+    // Print a string literal saying "Hello, World." to stdout using cout.   
+cout<<"Hello, World."<<endl<<input_string;
+
+    return 0;
+}
