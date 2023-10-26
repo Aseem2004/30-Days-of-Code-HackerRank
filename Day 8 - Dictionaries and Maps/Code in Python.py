9e3ba90c0c3265e987816febf571f0d29789fdf0
Aseem Jain <jainaseem2004@gmail.com>
2023-10-26 22:11:14 +0530

    Day 8 - In Python

---
+++ Day 8 - Dictionaries and Maps/Code in Python.py
@@ -0,0 +1,18 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+x = int(input())
+
+dictt = {}
+
+for i in range(x):
+    text = input().split()
+    dictt[text[0]] = text[1]
+
+while True:
+    try:
+        inpt = input()
+        if inpt in dictt:
+            print(inpt+"="+dictt[inpt])
+        else:
+            print("Not found")
+    except EOFError:
+        break
