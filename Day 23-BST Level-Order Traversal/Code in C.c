b47e47d64a6ae095e0bb0c04326bd5119559144e
Aseem Jain <jainaseem2004@gmail.com>
2024-01-26 17:23:34 +0530

    Day 23 - In C

---
+++ Day 23-BST Level-Order Traversal/Code in C.c
@@ -0,0 +1,75 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct Node{
+    struct Node* left;
+    struct Node* right;
+    int data;
+}Node;
+Node* newNode(int data){
+    Node* node=(Node*)malloc(sizeof(Node));
+    node->left=node->right=NULL;
+    node->data=data;
+    return node;
+}
+#define max(a, b) (a > b ? a : b)
+
+int getHeight(Node *root) {
+    if (root == NULL)
+        return 0;
+    else
+        return 1 + max(getHeight(root->left), getHeight(root->right));
+}
+
+void printGivenLevel(Node *root, int level) {
+    if (root == NULL)
+        return;
+    if (level == 1)
+        printf("%d ", root->data);
+    else if (level > 1)
+    {
+        printGivenLevel(root->left, level-1);
+        printGivenLevel(root->right, level-1);
+    } 
+}
+
+void levelOrder(Node* root){
+  //Write your code here
+    int height = getHeight(root);
+    int i;
+    for (i = 1; i <= height; i++) {
+        printGivenLevel(root, i);
+    }
+}
+
+Node* insert(Node* root,int data){
+    if(root==NULL)
+        return newNode(data);
+    else{
+        Node* cur;
+        if(data<=root->data){
+            cur=insert(root->left,data);
+            root->left=cur;                
+        }
+        else{
+            cur=insert(root->right,data);
+            root->right=cur;
+        }
+        
+    }
+    return root;
+}
+int main(){
+    Node* root=NULL;
+    int T,data;
+    scanf("%d",&T);
+    while(T-->0){
+        scanf("%d",&data);
+        root=insert(root,data);
+    }
+    levelOrder(root);
+    return 0;
+    
+}
